7a4e7948ae31ee299b1b8b003581148a8684af0b
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-26 21:02:18 +0200

    Cleanup.

--- spec/factories/ecm_videos_videos.rb
+++ spec/factories/ecm_videos_videos.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :ecm_videos_video, class: Ecm::Videos::Video do
     association(:category, factory: :ecm_videos_category)
     sequence(:name) { |i| "Video #{i}" }
-    clip { File.open(Ecm::Videos::Engine.root.join(*%w(spec files ecm_videos videos clip.mp4))) }
+    clip { File.open(Ecm::Videos::Engine.root.join(*%w(spec files ecm videos video clips example.mp4))) }
   end
 end
